d32852606ed33c1d29fd31d43670855e75f17251
sunsolo388 <79151613+sunsolo388@users.noreply.github.com>
2022-05-01 09:58:30 +0800

    11221

--- sunsolo_FR/urls.py
+++ sunsolo_FR/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from aftermarket import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('AMlist/', views.AMlist),
     path('AMlist/p1/', views.p1),
     path('AMlist/p2/',views.p1),
     path('AMlist/p3/',views.p3),
+    path('aftermarket/', include('aftermarket.urls')),
 ]
