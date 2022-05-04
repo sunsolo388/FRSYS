87e2f46156e9a3967d600dd47d5ee1e1ac7ff02f
Student-xtLin <1713079674@qq.con>
2022-05-04 22:11:15 +0800

    修改了外键的定义，优化了外键的级联删除（基于我个人猜测的需求），添加了related_name属性以便查找一对多关系中一所对应的多个对应值。

--- purchase/models.py
+++ purchase/models.py
@@ -9,10 +9,19 @@ class Purchase(models.Model):
     purchase 表
     '''
     purchase_id = models.CharField(max_length=8, primary_key=True, blank=True, verbose_name='采购编号')
-    deliver_id = models.ForeignKey(Deliver, on_delete=models.CASCADE,related_name="purchase_deliver")
+    deliver_id = models.ForeignKey(Deliver, on_delete=models.SET_NULL, related_name="purchase_of_this_deliver")
     purchase_num = models.FloatField(verbose_name='采购数量')
     purchase_time = models.DateTimeField(verbose_name='采购时间')
     purchase_price = models.FloatField(max_length=8, verbose_name='采购的价格')
+
+class PurchaseDetail(models.Model):
+    '''
+    purchase_detail 表
+    '''
+    purchase_id = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.DO_NOTHING, related_name='purchase_of_this_product')
+    supplier_id = models.ForeignKey('Supplier', on_delete=models.DO_NOTHING, related_name='purchase_of_this_supplier')
+    product_root = models.TextField()
 
 class Supplier(models.Model):
     '''
@@ -24,28 +33,10 @@ class Supplier(models.Model):
     supplier_charge_phone = models.PositiveBigIntegerField(verbose_name='供应商负责人电话')
     supplier_charge_name = models.CharField(max_length=20, blank=True, null=True, verbose_name='供应商负责人名字')
 
-class PurchaseDetail(models.Model):
-    '''
-    purchase_detail 表
-    '''
-    purchase_id = models.ForeignKey(Purchase, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    supplier_id = models.ForeignKey(Supplier, on_delete=models.CASCADE)
-    product_root = models.TextField()
-
-
 class SupplierDetail(models.Model):
     '''
     supplier_detail 表
     '''
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    supplier_id = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.DO_NOTHING, related_name='supplier_of_this_product')
+    supplier_id = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='detail_of_this_supplier')
 
-class PurchaseDetail(models.Model):
-    '''
-    purchase_detail 表
-    '''
-    purchase_id = models.ForeignKey(Purchase, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    supplier_id = models.ForeignKey(Supplier, on_delete=models.CASCADE)
-    product_root = models.TextField()
