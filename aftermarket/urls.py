c4785dd969d97a001cc876e8abb770d0574b7c4b
sunsolo388 <79151613+sunsolo388@users.noreply.github.com>
2022-05-06 16:33:49 +0800

    改了售后的表单内容引用，简要修改了下model和urls

--- aftermarket/urls.py
+++ aftermarket/urls.py
@@ -4,14 +4,8 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.index, name='index'),
+    path('/aftermarket/', views.AMtable, name='AMtable'),
 
-    # 例如: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
 
-    # 例如: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-
-    # 例如: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('/aftermarket/<int:AM_id>/', views.AMdealing, name='AMdealing')
 ]
