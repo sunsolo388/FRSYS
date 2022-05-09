2f2197f45ead6802cd098075a218c4a98a1493f5
sunsolo388 <79151613+sunsolo388@users.noreply.github.com>
2022-05-09 10:33:06 +0800

    数据库修改

--- FRSYS/aftermarket/models.py
+++ FRSYS/aftermarket/models.py
@@ -9,7 +9,8 @@ from django.db import models
 class AM(models.Model):
     AM_id=models.CharField(max_length=8)
     order_id=models.CharField(max_length=8)
-    reason_text=models.CharField(max_length=50)
+    reason_kind=models.CharField(max_length=10)
+    reason_detail=models.CharField(max_length=50)
     AM_state=models.CharField(max_length=8)
 
 
