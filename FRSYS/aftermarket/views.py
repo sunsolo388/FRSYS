5b759952279fea1e4e0e8022c878f06a3c8e123d
sunsolo388 <79151613+sunsolo388@users.noreply.github.com>
2022-05-15 21:12:34 +0800

    am基本功能实现，后续细节上可以进一步修改

--- FRSYS/aftermarket/views.py
+++ FRSYS/aftermarket/views.py
@@ -2,30 +2,54 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from aftermarket import models as amm
+
+
 # Create your views here.
 # 后台管理售后
 
 
 def AMtable(request):
     # 跳转到售后表单界面
-    if request.method =='GET':
-        try :
+    if request.method == 'GET':
+        try:
             aminfo = amm.AM.objects.all()
         except Exception as e:
-            aminfo = NULL
-
-
+            aminfo = None
+        # 此处可以留作扩展显示未查询到有处理信息怎么办
         return render(request, 'aftermarket/AMtable.html', locals())
     else:
-        am_id=request.POST.get("am_id")
-        AMinfo=amm.AM.objects.all()
-        return AMdealing(request,am_id)
+        am_id = request.POST.get("am_id")
+
+        return redirect(AMdealing, am_id=am_id)
 
 
 def AMdealing(request, am_id):
     # 跳转到处理详情界面
 
     if request.method == 'GET':
-        AM_id=am_id
-        return render(request, 'aftermarket/AMdealing.html',locals())
+        AMID = am_id
+        aminfo = amm.AM.objects.all()
+        orderID = None
+        reasontype = None
+        AMdetails = None
+        for i in aminfo:
+            if i.AM_id == AMID:
+                orderID = i.order_id
+                reasontype = i.reason_kind
+                AMdetails = i.reason_detail
+        return render(request, 'aftermarket/AMdealing.html', locals())
 
+    else:
+
+        dealingtext = request.POST.get('dealtext')
+        dealresult = request.POST.get('optionsRadios')
+        if dealresult == 0:
+            dealresult = '退款'
+        elif dealresult == 1:
+            dealresult = '不予退款'
+        amevent = amm.AM.objects.get(AM_id=am_id)
+        amevent.AM_status = '已处理'
+        amevent.save()
+
+
+        return redirect('/work/aftermarket')
--- FRSYS/sunsolo_FR/urls.py
+++ FRSYS/sunsolo_FR/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 from django.views.generic.base import RedirectView
 
@@ -25,67 +25,65 @@ from order.views import *
 from aftermarket.views import *
 from warehouse.views import *
 
-
 urlpatterns = [
-    path('',index),
+    path('', index),
     path('admin/', admin.site.urls),
 
     # homepage_yly
-    path('favicon.ico',RedirectView.as_view(url=r'static/favicon.ico')),
-    path('index/',index),
-    path('login/',login),
-    path('login/register/',register),
-    path('innerlogin/',login_worker),
-    path('innerlogin/innerregister/',register_worker),
-    path('index/about/',about),
-    path('index/services/',services),
-    path('index/contact/',contact),
-    path('work/',work),
-    path('userpage/',userpage),
+    path('favicon.ico', RedirectView.as_view(url=r'static/favicon.ico')),
+    path('index/', index),
+    path('login/', login),
+    path('login/register/', register),
+    path('innerlogin/', login_worker),
+    path('innerlogin/innerregister/', register_worker),
+    path('index/about/', about),
+    path('index/services/', services),
+    path('index/contact/', contact),
+    path('work/', work),
+    path('userpage/', userpage),
 
     # delivery_yly
-    path('work/delivery/',deliver_home),
-    path('work/delivery/glc/sfyz/',deliver_glc_sfyz),
-    path('work/delivery/glc/xqgl/',deliver_glc_xqgl),   
-    path('work/delivery/glc/rwfp/',deliver_glc_rwfp),
-    path('work/delivery/glc/jxz/',deliver_glc_jxz),
-    path('work/delivery/glc/ywc/',deliver_glc_ywc),
+    path('work/delivery/', deliver_home),
+    path('work/delivery/glc/sfyz/', deliver_glc_sfyz),
+    path('work/delivery/glc/xqgl/', deliver_glc_xqgl),
+    path('work/delivery/glc/rwfp/', deliver_glc_rwfp),
+    path('work/delivery/glc/jxz/', deliver_glc_jxz),
+    path('work/delivery/glc/ywc/', deliver_glc_ywc),
 
-    path('work/delivery/psc/sfyz/',deliver_psc_sfyz),
-    path('work/delivery/psc/<str:staff_id>/dqrw/',deliver_psc_dqrw),
-    path('work/delivery/psc/<str:staff_id>/xxsc/',deliver_psc_xxsc),
-    path('work/delivery/psc/<str:staff_id>/ywc/',deliver_psc_ywc),
+    path('work/delivery/psc/sfyz/', deliver_psc_sfyz),
+    path('work/delivery/psc/<str:staff_id>/dqrw/', deliver_psc_dqrw),
+    path('work/delivery/psc/<str:staff_id>/xxsc/', deliver_psc_xxsc),
+    path('work/delivery/psc/<str:staff_id>/ywc/', deliver_psc_ywc),
 
-    path('work/delivery/test/',test),
+    path('work/delivery/test/', test),
 
     # order_heyueyu
-    path('work/sales/',sales_home),
-    path('work/sales/order_check/',sales_order_check),
-    path('work/sales/customer_rela/',customer_manage),
-    path('work/purchase/order_stats/',order_stats),
-
+    path('work/sales/', sales_home),
+    path('work/sales/order_check/', sales_order_check),
+    path('work/sales/customer_rela/', customer_manage),
+    path('work/purchase/order_stats/', order_stats),
 
     # aftermarket_dyq
-    path('work/aftermarket/',AMtable),
-    path('work/aftermarket/<int:am_id>/',AMdealing),
+    path('work/aftermarket/', AMtable),
+    path('work/aftermarket/<am_id>/', AMdealing),
 
-    #warehouse_wxt
-    path('work/warehouse/',warehouse_home),
-    path('work/warehouse/inward',warehouse_inward),
-    path('work/warehouse/outward',warehouse_outward),
+    # warehouse_wxt
+    path('work/warehouse/', warehouse_home),
+    path('work/warehouse/inward', warehouse_inward),
+    path('work/warehouse/outward', warehouse_outward),
 
-    #purchase_lxt
-    path('work/purchase/',purchase_home),
+    # purchase_lxt
+    path('work/purchase/', purchase_home),
 
-    path('work/purchase/make_purchase/',purchase_make_purchase),
-    path('work/purchase/make_purchase/add/',purchase_make_purchase_add),
-    path('work/purchase/make_purchase/update/',purchase_make_purchase_update),
-    
-    path('work/purchase/manage_supplierinfo/',purchase_manage_supplierinfo),
-    path('work/purchase/manage_supplierinfo/add',purchase_manage_supplierinfo_add),
-    path('work/purchase/manage_supplierinfo/update',purchase_manage_supplierinfo_update),
+    path('work/purchase/make_purchase/', purchase_make_purchase),
+    path('work/purchase/make_purchase/add/', purchase_make_purchase_add),
+    path('work/purchase/make_purchase/update/', purchase_make_purchase_update),
 
-    path('work/purchase/purchase_demand/',purchase_purchase_demand),
-    path('work/purchase/purchase_demand/update',purchase_purchase_demand_update),
+    path('work/purchase/manage_supplierinfo/', purchase_manage_supplierinfo),
+    path('work/purchase/manage_supplierinfo/add', purchase_manage_supplierinfo_add),
+    path('work/purchase/manage_supplierinfo/update', purchase_manage_supplierinfo_update),
+
+    path('work/purchase/purchase_demand/', purchase_purchase_demand),
+    path('work/purchase/purchase_demand/update', purchase_purchase_demand_update),
 
 ]
