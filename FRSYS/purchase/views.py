d9833d129075971f650cad174daa073730978acd
Student-xtLin <1713079674@qq.con>
2022-05-22 01:02:50 +0800

    对df_xxx系列的url进行了更改，把溯源过程的后端写到了purchase的views.py中，可以翻到看。其中存在很多的问题，但是所有涉及到的实例我都以展示，具体要怎么处理待商量。

--- FRSYS/purchase/views.py
+++ FRSYS/purchase/views.py
@@ -319,7 +319,7 @@ def purchase_purchase_demands(request):
         elif 'update_purchase_demand_state' in request.POST:
             purchase_demand_id = request.POST.get('table_purchase_demand_id')
             purchase_demand = PurchaseDemand.objects.get(pdemand_id=purchase_demand_id)
-            # 修改采购需求状态
+            # 修改采购需求状态 可优化
             if purchase_demand.pdemand_state:
                 purchase_demand.pdemand_state = 0
             else:
@@ -330,3 +330,53 @@ def purchase_purchase_demands(request):
 
 
     return render(request,'purchase/purchase_demand/homepage.html', context=context)
+
+
+''' 溯源功能的实现-后端 '''
+from order.models import Order,OrderDetail
+from warehouse.models import Inward,WareHouse,Outward
+from deliver.models import Deliver,DeliverDetail
+from purchase.models import Purchase,PurchaseDetail,Supplier,SupplierDetail
+def order_trace(request):
+    if request.method == 'POST':
+        if 'search_order_trace' in request.POST:
+            order_id = request.POST.get('order_id') # 这里看前端到底把这个名字设成啥了
+
+            if order_id:    # 保证有输入内容
+                try:
+                    # Order实例
+                    order = Order.objects.get(order_id = order_id)
+                    '''  订单运输过程溯源  '''
+                    # Deliver实例
+                    deliver_order = order.deliver_id
+                    # DeliverDetail查询集  这里还需要考虑一个问题，如果这个订单还没发货，从而导致没有DeliverDetail呢
+                    deliver_details_order = DeliverDetail.objects.filter(deliver_id_id = deliver_order.deliver_id)
+                    '''  采购过程溯源  '''
+                    # OrderDetail查询集
+                    order_details = OrderDetail.objects.filter(order_id = order_id)
+                    '''  这里的业务逻辑需要沟通  前端需要啥数据 理论上，后端到这儿可以结束了'''
+                    for order_detail in order_details:
+                        # Outward实例
+                        outward = order_detail.outward_id
+                        # Inward实例
+                        inward = outward.warehouse_flow
+                        ''' 如果还需要Warehouse实例的话，可以用下面这个 不过这个就没办法在前端做到了。后端好像也没法搞'''
+                        # 这里获得的是一个WareHouse的查询集，所以我不推荐展示里面的数据
+                        # warehouse = WareHouse.objects.filter(warehouse_flow = inward.warehouse_flow)
+                        # Purchase实例
+                        purchase = inward.purchase_id
+                        # PurchaseDetail实例  这里没法在前端完成，所以，还需要进行讨论，该怎么完成
+                        purchase_detail = PurchaseDetail.objects.get(purchase_id_id=purchase.purchase_id)
+                        supplier = purchase_detail.supplier_id
+                        ''' 采购运输过程的溯源 '''
+                        # Deliver实例
+                        deliver_purchase = purchase.deliver_id
+                        # DeliverDetail查询集 这里没法在前端完成，所以，还需要进行讨论，该怎么完成
+                        deliver_details_purchase = DeliverDetail.objects.filter(deliver_id_id = deliver_purchase.deliver_id)
+
+                    '''  溯源过程到此结束  '''
+
+                except Order.DoesNotExist:
+                    messages.add_message(request, messages.ERROR, '查询失败！不存在该订单编号！')
+
+    return # 搞不清现在啥情况,返回哪个页面也不明白。前端来搞
